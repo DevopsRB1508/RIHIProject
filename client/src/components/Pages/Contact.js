238e97e5e45815caeda6946428dc19e414824cf6
ranganathan-a <ranganathan21rp@gmail.com>
2026-04-19 13:31:00 +0530

    URL Changes

--- client/src/components/Pages/Contact.js
+++ client/src/components/Pages/Contact.js
@@ -70,10 +70,10 @@ const Contact = () => {
     setIsSubmitting(true);
 
     try {
-      const response = await fetch('http://localhost:5000/api/contact', {
+      const response = await fetch('https://rajalaxshmiinteriors.com/api/contact', {
         method: 'POST',
         headers: {
-          'Content-Type': 'application/json',
+          'Content-Type': 'application/json'
         },
         body: JSON.stringify(formData),
       });
